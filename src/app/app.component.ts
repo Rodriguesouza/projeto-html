bdea731efac867d30eb977dca7a64198b32e4a16
Rodriguesouza <tiano.rsouza@hotmail.com>
2024-03-12 18:45:18 -0300

    exemplos de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'projeto-html';
-}
+
+ inputText = "Texto Inicial Alterado";
+ inputType = "text"
+ isDisabled = true;
+
+
+ enableInput() {
+
+    this.isDisabled = false;
+    
+ }
+
+  disebleInput (){
+
+    this.isDisabled = true;
+  }
+
+  setPasswordTypeInput(){
+    
+    this.inputType = 'password';
+
+  }
+
+  setTextTypeInput() {
+    this.inputType = 'text';
+
+  }
+
+  logInputText() {
+
+    console.log(this.inputText);
+
+  }
+
+  handleInputEvent(event:Event) {
+    const currentText = (event.target as HTMLInputElement).value
+
+    console.log (currentText)
+  }
+
+  }
+
+
